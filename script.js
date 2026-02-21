1e52a5dc6d9c7417ff799ed672ca69588d2977ea
Felipe Ken <fkenhirano4@gmail.com>
2026-02-21 19:36:20 -0300

    feat: Created portfolio base

---
+++ script.js
@@ -0,0 +1,10 @@
+// HEADER SCROLL
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
